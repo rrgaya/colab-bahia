2f2f6a6e0276c585ea656c96511dff51776c3251
Ricardo Gaya <rrgaya@gmail.com>
2020-10-03 10:36:50 -0300

    Add database config and models

---
+++ app/database.py
@@ -0,0 +1,26 @@
+import os
+from decouple import config
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+DB_USER = config("DB_USER")
+DB_PASS = config("DB_PASS")
+HOST = config("HOST")
+PORT = config("PORT")
+DBNAME = config("DBNAME")
+
+# TODO
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{HOST}:{PORT}/{DBNAME}"
+
+if "DATABASE_URL" in os.environ:
+    ENV_DATABASE_URL = os.environ.get("DATABASE_URL")
+else:
+    ENV_DATABASE_URL = config("LOCAL_DATABASE_URL")
+
+engine = create_engine(ENV_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
---
+++ app/models.py
@@ -0,0 +1,20 @@
+from uuid import uuid4
+from sqlalchemy.dialects.postgresql import UUID
+
+from sqlalchemy import Column, Integer, Numeric, String
+from .database import Base
+
+
+class Fiscalizacao(Base):
+    __tablename__ = "fiscalizacao"
+
+    id = Column(UUID(as_uuid=True), primary_key=True,
+                index=True, default=uuid4)
+    # id = Column(Integer, primary_key=True, index=True)
+    # symbol = Column(String, unique=True, index=True)
+    # price = Column(Numeric(10, 2))
+    # forward_pe = Column(Numeric(10, 2))
+    # forward_eps = Column(Numeric(10, 2))
+    # dividend_yield = Column(Numeric(10, 2))
+    # ma50 = Column(Numeric(10, 2))
+    # ma200 = Column(Numeric(10, 2))
