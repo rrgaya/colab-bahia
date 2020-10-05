4005fdd312f6f930e9b15a3f6bc5b5b3c0d381ad
Ricardo Gaya <rrgaya@gmail.com>
2020-10-05 15:56:35 -0300

    Add project Django e removendo estrutura antiga em FastAPI

--- app/crud.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy.orm import Session
-from . import models, schemas
-
-
-def get_posts(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.Post).offset(skip).limit(limit).all()
