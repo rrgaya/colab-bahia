95b0db93a7ceea6b9db78b4b0c82faa9eb74dac3
Ricardo Gaya <rrgaya@gmail.com>
2020-10-05 18:56:00 -0300

    Adicionado foto a model de post e Pillow Closes #10

--- fiscalizacao/models.py
+++ fiscalizacao/models.py
@@ -16,6 +16,10 @@ class Category(BaseModel):
     def __str__(self) -> str:
         return str(self.name)
 
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+
 
 class SubCategory(BaseModel):
     name = models.CharField(max_length=200)
@@ -23,18 +27,24 @@ class SubCategory(BaseModel):
     def __str__(self) -> str:
         return str(self.name)
 
+    class Meta:
+        verbose_name = "Sub Categoria"
+        verbose_name_plural = "Sub Categorias"
+
 
 class Post(BaseModel):
     title = models.CharField(max_length=200)
-    category = models.ForeignKey(
-        Category, on_delete=models.CASCADE, null=True, blank=True)
-    sub_category = models.ForeignKey(
-        SubCategory, on_delete=models.CASCADE, null=True, blank=True)
+    photo = models.ImageField(upload_to="image-posts/", blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE, null=True, blank=True)
     address = models.CharField(max_length=200)
     description = models.TextField()
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     support_case = models.PositiveIntegerField()
 
     def __str__(self) -> str:
         return str(self.title)
+
+    class Meta:
+        verbose_name = "Post de fiscalização"
+        verbose_name_plural = "Post de fiscalizações"
