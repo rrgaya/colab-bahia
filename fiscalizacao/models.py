851e4216c7d6357d1add39dbd4b0f5981ecb74b3
Ricardo Gaya <rrgaya@gmail.com>
2020-10-05 16:34:54 -0300

    Add gunicorn e corrigindo bugs no models

--- fiscalizacao/models.py
+++ fiscalizacao/models.py
@@ -1,6 +1,6 @@
-from os import terminal_size
 from django.db import models
 from django.contrib.auth.models import User
+
 
 class BaseModel(models.Model):
     create_at = models.DateField(auto_created=True, auto_now_add=True)
@@ -9,11 +9,13 @@ class BaseModel(models.Model):
     class Meta:
         abstract = True
 
+
 class Category(BaseModel):
     name = models.CharField(max_length=200)
 
     def __str__(self) -> str:
         return str(self.name)
+
 
 class SubCategory(BaseModel):
     name = models.CharField(max_length=200)
@@ -21,13 +23,17 @@ class SubCategory(BaseModel):
     def __str__(self) -> str:
         return str(self.name)
 
+
 class Post(BaseModel):
     title = models.CharField(max_length=200)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
-    sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE, null=True, blank=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, null=True, blank=True)
+    sub_category = models.ForeignKey(
+        SubCategory, on_delete=models.CASCADE, null=True, blank=True)
     address = models.CharField(max_length=200)
     description = models.TextField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
     support_case = models.PositiveIntegerField()
 
     def __str__(self) -> str:
